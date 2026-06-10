3188e893bc2eab0de4411b5d9c8fda250a0e068f
Philipp <if25b147@technikum-wien.at>
2026-06-10 20:38:01 +0200

    Add test for Aged Brie actually increases in Quality the older it gets

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -4,13 +4,13 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
-    def test_expired_items_degrade_twice_as_fast(self):
-        items = [Item("foo", 0, 10)]
+    def test_aged_brie_increases_in_quality(self):
+        items = [Item("Aged Brie", 2, 0)]
 
         GildedRose(items).update_quality()
 
-        self.assertEqual(-1, items[0].sell_in)
-        self.assertEqual(8, items[0].quality)
+        self.assertEqual(1, items[0].sell_in)
+        self.assertEqual(1, items[0].quality)
 
 if __name__ == '__main__':
     unittest.main()
