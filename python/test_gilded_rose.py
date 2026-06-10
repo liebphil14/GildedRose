bf949fd90ba0f10777bba768c4b6c565615e00dc
Philipp <if25b147@technikum-wien.at>
2026-06-10 19:54:38 +0200

    Add test for Once the sell by date has passed, Quality degrades twice as fast

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -4,11 +4,13 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
-    def test_quality_never_negative(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("foo", items[0].name)
+    def test_expired_items_degrade_twice_as_fast(self):
+        items = [Item("foo", 0, 10)]
+
+        GildedRose(items).update_quality()
+
+        self.assertEqual(-1, items[0].sell_in)
+        self.assertEqual(8, items[0].quality)
 
 if __name__ == '__main__':
     unittest.main()
