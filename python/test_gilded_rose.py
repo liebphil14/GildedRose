769ad05815f3d22558087f85f5c4da10d50c7fa9
Philipp <if25b147@technikum-wien.at>
2026-06-10 20:45:52 +0200

    Add test for Aged Brie not exceeding Quality of 50

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,12 +5,11 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
     def test_aged_brie_increases_in_quality(self):
-        items = [Item("Aged Brie", 2, 0)]
+        items = [Item("Aged Brie", 2, 50)]
 
         GildedRose(items).update_quality()
 
-        self.assertEqual(1, items[0].sell_in)
-        self.assertEqual(1, items[0].quality)
+        self.assertEqual(50, items[0].quality)
 
 if __name__ == '__main__':
     unittest.main()
