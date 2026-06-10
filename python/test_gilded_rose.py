c0118954528e90905f3db6474f4d1bbd356b301e
Philipp <if25b147@technikum-wien.at>
2026-06-10 20:55:13 +0200

    Add test for Sulfuras has never to be sold or decrease in Quality

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,11 +5,11 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
     def test_aged_brie_increases_in_quality(self):
-        items = [Item("Aged Brie", 2, 50)]
+        items = [Item("Sulfuras, Hand of Ragnaros", 5, 80)]
 
         GildedRose(items).update_quality()
 
-        self.assertEqual(50, items[0].quality)
-
+        self.assertEqual(5, items[0].sell_in)
+        self.assertEqual(80, items[0].quality)
 if __name__ == '__main__':
     unittest.main()
