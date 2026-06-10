5e44f45c114294b68f5aa9ae483a94df5b2a0f44
Philipp <if25b147@technikum-wien.at>
2026-06-10 19:18:04 +0200

    Add test for non-negative quality

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -4,11 +4,11 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
+    def test_quality_never_negative(self):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual("foo", items[0].name)
 
 if __name__ == '__main__':
     unittest.main()
