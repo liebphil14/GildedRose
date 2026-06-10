1c73e01bee7c7da4d4cfcba389ed04fa604dacc6
Philipp <if25b147@technikum-wien.at>
2026-06-10 20:58:19 +0200

    Add test for Backstage passes increasing Quality by 1 when SellIn higher than 10

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,11 +5,10 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
     def test_aged_brie_increases_in_quality(self):
-        items = [Item("Sulfuras, Hand of Ragnaros", 5, 80)]
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)]
 
         GildedRose(items).update_quality()
 
-        self.assertEqual(5, items[0].sell_in)
-        self.assertEqual(80, items[0].quality)
+        self.assertEqual(21, items[0].quality)
 if __name__ == '__main__':
     unittest.main()
